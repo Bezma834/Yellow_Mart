03631691e278c2fbf834845ff94612d2eac9a369
Beza <bezawittenaw0@gmail.com>
2026-07-24 12:30:07 +0300

    Fix Render CORS

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,40 +10,37 @@ dotenv.config();
 const app = express();
 
 
-// CORS Configuration
 app.use(cors({
   origin: [
-    "https://yellow-mart-8hi9622el-yellow-mart.vercel.app",
     "https://yellow-mart-git-main-yellow-mart.vercel.app",
+    "https://yellow-mart-8hi9622el-yellow-mart.vercel.app",
     "http://localhost:3000"
   ],
   credentials: true
 }));
 
 
-// Read JSON requests
 app.use(express.json());
 
 
-// Authentication routes
 app.use(
   "/api/auth",
   authRoutes
 );
 
 
-// User routes
 app.use(
   "/api",
   userRoutes
 );
 
 
-// Test backend
 app.get("/", (req, res) => {
+
   res.send(
     "Yellow-Mart Backend Running 🚀"
   );
+
 });
 
 
